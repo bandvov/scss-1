94d77b4535f57c975af1496c5855f8b9474064f1
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-24 14:54:02 +0200

    add breadcrumb

--- src/components/breadcrumbs/BreadCrumbs.js
+++ src/components/breadcrumbs/BreadCrumbs.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import styles from './Breadcrumbs.module.scss'
 
 export default function BreadCrumbs() {
+  const location = useLocation()
+  const breadcrumbsArray = location.pathname.split('/').filter(Boolean)
+  const mappedBreadcrumbs = breadcrumbsArray.map((path, index) => {
+    const url = breadcrumbsArray.slice(0, index + 1)
+    return <li className={styles.breadcrumbs__item}>
+      <span className={styles.breadcrumbs__item__separator}>></span>
+      <NavLink
+        to={url.join('/')}
+        end
+        className={({ isActive }) =>
+          isActive ? styles.breadcrumbs__item__active : styles.breadcrumbs__item__notactive
+        }>
+        {path}
+      </NavLink>
+    </li>
+  })
+
   return (
-    <div>BreadCrumbs</div>
+    <ul className={styles.breadcrumbs}>
+      <li className={styles.breadcrumbs__item}>
+        <NavLink to={'/'}>
+          Home
+        </NavLink>
+      </li>
+      {mappedBreadcrumbs}
+    </ul>
   )
 }
