e7952b9dd018484f990d5a5c9b3677a74f1e7a91
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-25 15:31:24 +0200

    add constants

--- src/constants.js
+++ src/constants.js
@@ -5,19 +5,65 @@ export const HEADER_MENU_ITEMS = [
   },
   {
     title: "Explore",
-    url: "/1"
+    url: "/explore"
   },
   {
     title: "Showroom",
-    url: "/2"
+    url: "/showroom"
   },
   {
     title: "Credit Simulation",
-    url: "/3"
+    url: "/credit-simulation"
   },
   {
     title: "Contact Us",
-    url: "/4"
+    url: "/contact-us"
   },
 ]
 
+export const SPECIFICATION_ITEMS = [
+  {
+    name: "Mileage",
+    value: 123
+  },
+  {
+    name: "Engine",
+    value: 123
+  },
+  {
+    name: "Year",
+    value: 123
+  },
+  {
+    name: "Fuel Comsuption",
+    value: 123
+  },
+  {
+    name: "Fuel type",
+    value: "Electric"
+  },
+  {
+    name: "Fuel Economy",
+    value: 12
+  },
+  {
+    name: "Exterior Color",
+    value: "Gray"
+  },
+  {
+    name: "Interior Color",
+    value: "Jet Black"
+  },
+  {
+    name: "Transmission",
+    value: "Active"
+  },
+  {
+    name: "Power",
+    value: "N/A"
+  },
+  {
+    name: "Drive",
+    value: "4 WD"
+  },
+]
