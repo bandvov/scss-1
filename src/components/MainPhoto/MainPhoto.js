8ea7a93c61ef850c6cfcd3d4e567a3f0ffeb5141
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-25 17:41:45 +0200

    made main image dynamic

--- src/components/MainPhoto/MainPhoto.js
+++ src/components/MainPhoto/MainPhoto.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MainPhoto.module.scss'
 
 export default function MainPhoto() {
+  const [activeImage, setActiveImage] = useState(0)
+  const imagesArr = [
+    {
+      src: 'https://assets.einhell.com/im/imf/y400/900_493663/productimage'
+    },
+    {
+      src: 'https://assets.einhell.com/im/imf/y400/900_493673/detail_image'
+    },
+    {
+      src: ''
+    },
+  ]
   return (
     <section className={styles.mainphoto}>
-      <div className={styles.bigphoto}></div>
-      <div className={styles.smallphoto}></div>
-      <div className={styles.smallphoto}></div>
-      <div className={styles.smallphoto}></div>
+      <div className={styles.bigphoto}>
+        <img style={{
+          width: "100%",
+          height: '100%',
+          objectFit: 'contain',
+        }}
+          src={imagesArr[activeImage].src} />
+      </div>
+
+      {imagesArr.map((image, index) => {
+        return <div onClick={() => setActiveImage(index)} className={styles.smallphoto}>
+          <img style={{
+            width: "100%",
+            objectFit: 'cover',
+          }}
+            {...image} />
+        </div>
+      })}
     </section>
   )
 }
