e3ece9c4ce4a0df9d40c2436363a3cbe1819f82f
Volodymyr Vasylkiv <bandvov@gmail.com>
2023-01-02 19:17:19 +0200

    fixed small style issues

--- src/components/FeaturedCarItem/FeaturedCarItem.js
+++ src/components/FeaturedCarItem/FeaturedCarItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './FeaturedCarItem.module.scss'
 export default function FeaturedCarItem({
   name,
@@ -9,16 +10,18 @@ export default function FeaturedCarItem({
 }) {
 
   return (
-    <li className={styles.featured__car} {...props}>
-      <img alt={name} className={styles.featured__car__image} src={src} />
-      <div className={styles.featured__car__content} >
-        <h2 alt={name} className={styles.featured__car__name}>
-          {name}
-        </h2>
-        <h2 className={styles.featured__car__price}>
-          ${price}
-        </h2>
-      </div>
+    <li className={styles.featured__car__list__item}  {...props}>
+      <Link to={url} className={styles.featured__car}>
+        <img alt={name} className={styles.featured__car__image} src={src} />
+        <div className={styles.featured__car__content} >
+          <h2 alt={name} className={styles.featured__car__name}>
+            {name}
+          </h2>
+          <h2 className={styles.featured__car__price}>
+            ${price}
+          </h2>
+        </div>
+      </Link>
     </li>
   )
 }
